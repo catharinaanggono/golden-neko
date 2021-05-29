44a8c02ca8463859320903902e29c6c6556107ba
catharinaanggono <79496983+catharinaanggono@users.noreply.github.com>
2021-05-29 16:32:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function move(name){
     localStorage.setItem("name", name);
-    window.location.href = 'booking.html';
+    window.location.href = 'booking.php';
 }
 
 function move_movie(movie_name){
     localStorage.setItem("movie_name", movie_name);
-    window.location.href = 'booking.html';
+    window.location.href = 'booking.php';
 }
